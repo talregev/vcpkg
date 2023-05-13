3f28b5f34481ff88552ae390436ada3216363d9c
Kai Pastor <dg0yt@darc.de>
2023-05-13 13:06:17 +0200

    Cleanup

--- ports/sdformat13/portfile.cmake
+++ ports/sdformat13/portfile.cmake
@@ -1,8 +1,7 @@
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gazebosim/sdformat
-    REF sdformat13_${VERSION}
+    REF "sdformat13_${VERSION}"
     SHA512 180e28631c02d92bfd17d31ec32edb234e6c56c8fad011d04198481a39bffc26355b9001c598fa63a30ce47ad8abaeaed05e3008957c5e77df3a09b0170f0e6d
     HEAD_REF sdf13
     PATCHES
@@ -15,22 +14,21 @@ vcpkg_find_acquire_program(RUBY)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS 
-        -DBUILD_TESTING=OFF
-        -DUSE_INTERNAL_URDF=OFF
-        -DUSE_EXTERNAL_TINYXML=ON
+    OPTIONS
         "-DRUBY=${RUBY}"
+        -DBUILD_TESTING=OFF
+        -DSKIP_PYBIND11=ON
+        -DUSE_INTERNAL_URDF=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON
+        -DCMAKE_REQUIRE_FIND_PACKAGE_GzURDFDOM=ON
+        -DCMAKE_DISABLE_FIND_PACKAGE_Python3=ON
 )
 
 vcpkg_cmake_install()
-
-# Fix cmake targets and pkg-config file location
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/sdformat13")
 vcpkg_fixup_pkgconfig()
 
-# Remove debug files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
